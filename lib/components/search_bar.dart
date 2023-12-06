f026ff38fce777107c4b32e7118cad2b84a271f6
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-12-06 16:32:13 +0100

    add the title and subtitle on the video

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchBarApp extends StatefulWidget {
+  const SearchBarApp({super.key});
+
+  @override
+  State<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends State<SearchBarApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/components/video_hero.dart
+++ lib/components/video_hero.dart
@@ -1,21 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-
 class VideoHero extends StatefulWidget {
   const VideoHero({super.key});
-
 
   @override
   State<VideoHero> createState() => _VideoHeroState();
 }
 
 class _VideoHeroState extends State<VideoHero> {
-
   late VideoPlayerController _controller;
 
-   @override
+  @override
   void initState() {
     super.initState();
     // Pointing the video controller to our local asset.
@@ -31,48 +27,60 @@ class _VideoHeroState extends State<VideoHero> {
   }
 
   @override
+  Widget build(BuildContext context) {
+    // Complete the code in the next step.
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.red,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            // Use the VideoPlayer widget to display the video.
+            child: Stack(
+              children: <Widget>[
+                VideoPlayer(_controller),
+                const Positioned(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "France Data",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Courier new',
+                        fontSize: 35,
+                      ),
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Atlas des donnés de France",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Courier new',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
   void dispose() {
     // Ensure disposing of the VideoPlayerController to free up resources.
     _controller.dispose();
 
     super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Complete the code in the next step.
-    return Container(
-      decoration: const BoxDecoration(
-         color: Colors.red,
-       ),
-      child : Column(
-         mainAxisAlignment: MainAxisAlignment.
-           spaceEvenly,
-        children: [
-             const Text(
-           "France Data",
-           style:  TextStyle(
-             color: Colors.white,
-             fontFamily: 'Courier new',
-             fontSize: 35,
-           ),
-         ),
-              const Text(
-           "Atlas des données de France",
-           style:  TextStyle(
-             color: Colors.white,
-             fontFamily: 'Courier new',
-             fontSize: 15,
-           ),
-         ),
-         AspectRatio(
-               aspectRatio: _controller.value.aspectRatio,
-               // Use the VideoPlayer widget to display the video.
-               child: VideoPlayer(_controller),
-             ),
-        ],
-      ),
-    
-    );
   }
 }
